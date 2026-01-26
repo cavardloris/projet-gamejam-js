74c120c5599fad8fc10d4c6aac410b00470404db
cavardloris <cavardloris@gmail.com>
2026-01-26 10:49:50 +0100

    Création de duck.js et quelques modif dans gameEntity

--- GAME JAM JS/src/Class/Duck.js
+++ GAME JAM JS/src/Class/Duck.js
@@ -1,8 +1,32 @@
 import { GameEntity } from "./GameEntity";
 
-class Duck extends GameEntity{
-    constructor(x,y){
-        super();
+class Duck extends GameEntity {
+  constructor(x, y) {
+    super(x, y, 50, 150);
 
+    this.velocity = 0;
+    this.gravity = 0.25;
+    this.jumpStrength = 4.5;
+
+    // creation de l'image du canard
+    this.image = new Image();
+    this.image.src = "./assets/flappyDuck.png";
+  }
+
+  update() {
+    this.velocity += this.gravity;
+    this.y += this.velocity; // peut etre considéré comme la fonction de chute libre
+  }
+
+  jump() {
+    this.velocity = -this.jumpStrength; // le saut
+  }
+
+  draw(ctx) {
+    if (this.image.complete) {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    } else {
+      super.draw(ctx);
     }
+  }
 }
